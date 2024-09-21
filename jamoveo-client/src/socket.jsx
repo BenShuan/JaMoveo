5d2c78e028a9d265e79126099e05e860f158e917
Ben Shuan <benshuan@gmail.com>
2024-09-22 00:31:10 +0300

    Finished

--- jamoveo-client/src/socket.jsx
+++ jamoveo-client/src/socket.jsx
@@ -1,6 +1,3 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : import.meta.env.VITE_APP_SERVER_URL;
-
-export const socket = io(URL);
+export const socket = io("wss://immense-bayou-69622-27b3bda0f510.herokuapp.com");
